456b6f87c27e7445c981db82b779941f19187fdb
Tharbouch <112890451+soufianeargane@users.noreply.github.com>
2023-10-14 00:16:08 +0100

    seperated sending mail code to sendMail.js in the helper directory

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,9 @@
 const { validateForms } = require('../validators/validateUserForms');
+const sendMail = require('../helpers/sendMail');
 
 const UserModel = require('../models/UserModel');
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const nodemailer = require("nodemailer");
 require('dotenv').config();
 
 async function register (req, res) {
@@ -27,27 +27,8 @@ async function register (req, res) {
     // generate a token with 600 seconds of expiration
     const token = jwt.sign(payload, process.env.TOKEN_SECRET, { expiresIn: 600});
 
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.ethereal.email',
-        port: 587,
-        auth: {
-            user: 'monserrate.oberbrunner21@ethereal.email',
-            pass: 'VaNWfN9Vwwt7HD1DSw'
-        }
-    });
-
-    // define email options
-    let mailOptions = {
-        from: 'AlloMedia.livraieon@media.com',
-        to: req.body.email,
-        subject: 'Account activation link',
-        text: `Hello ${req.body.name},`,
-        html: `<h1> Please click on the link to activate your account </h1>
-        <a href="http://localhost:3000/api/auth/activate/${token}">Activate your account</a>`,
-    };
-
-    let info = await transporter.sendMail(mailOptions);
-    console.log("Message sent: %s", info.messageId);
+    // send email
+    sendMail(req.body, token);
     return res.json({ token });
 
     // Create a new user
---
+++ helpers/sendMail.js
@@ -0,0 +1,33 @@
+const nodemailer = require("nodemailer");
+
+async function sendMail(info, token){
+    try {
+        const transporter = nodemailer.createTransport({
+            host: 'smtp.ethereal.email',
+            port: 587,
+            auth: {
+                user: 'monserrate.oberbrunner21@ethereal.email',
+                pass: 'VaNWfN9Vwwt7HD1DSw'
+            }
+        });
+
+        // define email options
+        let mailOptions = {
+            from: 'AlloMedia.livraieon@media.com',
+            to: info.email,
+            subject: 'Account activation link',
+            text: `Hello ${info.name},`,
+            html: `<h1> Please click on the link to activate your account </h1>
+        <a href="http://localhost:3000/api/auth/activate/${token}">Activate your account</a>`,
+        };
+
+        let details = await transporter.sendMail(mailOptions);
+        console.log("Message sent: %s", details.messageId);
+
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+}
+
+module.exports = sendMail;

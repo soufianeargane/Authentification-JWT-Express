090bb1becc60d1c08523c69098b929be764b82b7
Tharbouch <112890451+soufianeargane@users.noreply.github.com>
2023-10-16 16:09:15 +0100

    just did 3 tests working well for active function

--- tests/active.test.js
+++ tests/active.test.js
@@ -1,5 +1,7 @@
 const active = require('../controllers/authController').activate;
 const validateToken = require('../validators/validateToken');
+const UserModel= require('../models/UserModel');
+
 
 jest.mock('../validators/validateToken');
 
@@ -48,7 +50,36 @@ describe('Test activate function', () => {
 
     });
 
+    it('should return 200 if token is valid', async () => {
 
+        validateToken.mockReturnValue({
+            success: 'Token is valid',
+            data: {
+                _id: '652d4f9a79e246509057fbd0',
+            }
+        });
+        UserModel.updateOne = jest.fn().mockResolvedValue({ nModified: 1 });
+        const req = {
+            params: {
+                token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoic291ZmlhbmUiLCJlbWFpbCI6ImFub3ZpY3Nvc29AZ21haWwuY29tIiwiaXNfdmVyaWZpZWQiOmZhbHNlLCJyb2xlIjoiNjUyYWUzNDNjM2I3NTE1N2I0OGVkN2IzIiwiX2lkIjoiNjUyZDRmOWE3OWUyNDY1MDkwNTdmYmQwIiwiZGF0ZSI6IjIwMjMtMTAtMTZUMTQ6NTg6MzQuNTg5WiIsIl9fdiI6MCwiaWF0IjoxNjk3NDY4MzE0LCJleHAiOjE2OTc0Njg5MTR9.JiTOhI1X6YDRAhPFDUGCRIzXmg5rMuZv-8YZdEiV6fk', // Provide a valid token
+            }
+        };
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        };
+
+        await active(req, res);
+
+        expect(UserModel.updateOne).toHaveBeenCalledWith(
+            { _id: '652d4f9a79e246509057fbd0' }, // Replace with the expected user ID
+            { is_verified: true }
+        );
+
+        // Check if res.json is called with the success message
+        expect(res.json).toHaveBeenCalledWith({ success: 'Account activated successfully' });
+
+    });
 
 
 });
